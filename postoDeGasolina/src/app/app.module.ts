d1c493271a3180f2684c7dd25c0df2a064d57687
Pablo Teyssiere <teyssiere.lupab24@triana.salesianos.edu>
2024-11-04 12:27:11 +0100

    add: postoDeGasolina

---
+++ postoDeGasolina/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GasolinaListComponent } from './components/gasolina-list/gasolina-list.component';
+import { GasolinaCardComponent } from './components/gasolina-card/gasolina-card.component';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GasolinaListComponent,
+    GasolinaCardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ postoDeGasolina/src/app/components/gasolina-list/gasolina-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gasolina-list',
+  templateUrl: './gasolina-list.component.html',
+  styleUrl: './gasolina-list.component.css'
+})
+export class GasolinaListComponent {
+
+}
